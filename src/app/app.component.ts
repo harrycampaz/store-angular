2e05dba5dbef9e5f4c7be2908f4a6086bd1ff531
harrycampaz <harrycampaz@gmail.com>
2020-04-20 20:48:05 -0500

    Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './model/product';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store-dezzapps';
+
+  products: Product[] = [{
+    id: 1,
+    image: 'assets/images/camiseta.png',
+    title: 'Camiseta',
+    price: 800000,
+    description: 'La descrion'
+  },
+  {
+    id: 2,
+    image: 'assets/images/hoodie.png',
+    title: 'Camiseta',
+    price: 800000,
+    description: 'La descrion'
+  },
+  {
+    id: 3,
+    image: 'assets/images/mug.png',
+    title: 'Camiseta',
+    price: 800000,
+    description: 'La descrion'
+  },
+  {
+    id: 4,
+    image: 'assets/images/pin.png',
+    title: 'Camiseta',
+    price: 800000,
+    description: 'La descrion'
+  }];
+
+
+  clickProduct(id: number) {
+    console.log('Clic product');
+    console.log(id);
+
+  }
+
 }
